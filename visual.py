9e20a463899cd1525d4f443c45624f2cd24b2e57
Martin Ruefenacht <m.ruefenacht@ed.ac.uk>
2016-07-01 12:03:56 +0100

    Added seperate visualization to tasks, core-visual seperated

---
+++ visual.py
@@ -0,0 +1,81 @@
+import cairo, math
+from tasks import *
+
+def outputPDF(filename, machine, program):
+	# calculate resolution
+	xmargin = 10
+	ymargin = 20
+	pheight = 36
+	cheight = 4
+	theight = 2
+	scale = 1/10
+
+	height = ymargin * 2 + pheight * (machine.size-1)
+	width  = xmargin * 2 + int(math.ceil(max(machine.procs) / 1000.0)) * 1000 * scale
+
+	# create surface
+	pdf = cairo.PDFSurface(filename, width, height) 
+
+	# create context
+	ctx = cairo.Context(pdf)
+
+	# set settings
+	ctx.set_antialias(cairo.ANTIALIAS_GRAY)
+	ctx.set_source_rgb(0, 0, 0)
+	ctx.set_line_width(0.5)
+	# scale to time tick resolution
+	ctx.set_font_size(4)
+
+	# draw process lines
+	for proc in range(machine.size):
+		ctx.move_to(xmargin, ymargin + proc * pheight) 
+		ctx.rel_line_to((width - xmargin * 2), 0)
+		ctx.stroke()
+
+	# draw timeline
+	ctx.move_to(xmargin, ymargin/3)
+	ctx.rel_line_to((width-xmargin*2), 0)
+	ctx.stroke()	
+
+	for tick in range(int((width - xmargin*2)/10)+1):
+		if tick % 5 == 0:
+		    # draw number
+			ctx.move_to(xmargin + tick / scale - 2, ymargin/3 + 8)
+			ctx.show_text(str(tick*100))
+
+		# draw ticks
+		ctx.move_to(xmargin + tick / scale, ymargin/3)
+		ctx.rel_line_to(0, 4)
+		ctx.stroke()
+	
+	# draw program
+	for node, data in program.nodes_iter(data=True):
+		task = data['task']
+
+		if isinstance(task, StartTask):
+			pass
+
+		elif isinstance(task, PutTask):
+			# draw put task
+			# draw local block
+			ctx.rectangle(xmargin + task.time * scale, ymargin + pheight * task.proc - theight/2, task.local * scale, theight) 
+			ctx.fill()
+			
+			# draw transfer
+			direct = 3 if (task.target - task.proc) > 0 else -3
+
+			ctx.move_to(xmargin + task.time * scale + task.local * scale, ymargin + pheight * task.proc)
+			ctx.rel_line_to(0, direct)
+			ctx.line_to(xmargin + task.time * scale + task.remote * scale, ymargin + pheight * task.target - direct)
+			ctx.rel_line_to(0, direct)
+			ctx.stroke()
+
+		elif isinstance(task, ComputeTask):
+			# draw compute task
+			ctx.rectangle(xmargin + task.time*scale, ymargin + pheight * task.proc - cheight/2, task.delay*scale, cheight) 
+			ctx.fill()
+			
+		else:
+			print('Unknown task type:', task)
+
+			

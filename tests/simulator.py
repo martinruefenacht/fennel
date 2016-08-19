1ea8524c8ac48f5054ae1cd23375d6cf00298de4
Martin Ruefenacht <m.ruefenacht@ed.ac.uk>
2016-08-19 17:19:13 +0100

    Revamp

--- tests/simulator.py
+++ tests/simulator.py
@@ -5,22 +5,26 @@ import sys, parser, lbmachine, logpmachine, visual
 if __name__ == "__main__":
 	# parse program
 	program = parser.parseGOAL(sys.argv[1])
+	# TODO by pipe
 
 	# create machine for program
-	#machine = lbmachine.LBPMachine(program, 500, 0, 400)
+	machine = lbmachine.LBPMachine(program, 1000, 0, 400)
+
+
 	#machine = lbmachine.LBMachine(program, 700, 0)
-	machine = logpmachine.LogPMachine(program)
+	#machine = logpmachine.LogPMachine(program)
 
 	# set noise
 	#machine.host_noise = True
 	#machine.network_noise = True
 
 	# set visual
-	visual = visual.Visual()
-	machine.setVisual(visual)
+	#visual = visual.Visual()
+	#machine.setVisual(visual)
 
 	machine.run()
 
+	# TODO which metric evaluated
 	print(machine.procs)
 
-	visual.savePDF('test.pdf')
+	#visual.savePDF('test.pdf')

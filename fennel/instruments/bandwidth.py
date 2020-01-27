1ede616a719016ab5d0c605832907c190dec67fa
Martin Ruefenacht <m.a.ruefenacht@gmail.com>
2020-01-27 12:50:39 +0000

    Added stub for bandwidth instrument

---
+++ fennel/instruments/bandwidth.py
@@ -0,0 +1,17 @@
+"""
+"""
+
+
+from fennel.core.instrument import Instrument
+
+
+class BandwidthInstrument(Instrument):
+    """
+    """
+
+    def __init__(self):
+        pass
+
+
+    def task_executed(self):
+        pass

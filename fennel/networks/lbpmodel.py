e297590281d10d10f44c85649b19ace385da56bc
Martin Ruefenacht <m.a.ruefenacht@gmail.com>
2020-05-03 23:12:53 +0100

    Added beginning of handling Get with LBP

--- fennel/networks/lbpmodel.py
+++ fennel/networks/lbpmodel.py
@@ -3,12 +3,13 @@ Defines the latency-bandwidth-pipeline model.
 """
 
 
-from typing import cast
+from typing import cast, Union
 
 
 from fennel.core.time import Time
 from fennel.core.network import NetworkModel, NetworkTime
 from fennel.tasks.put import PutTask
+from fennel.tasks.get import GetTask
 
 
 class LBPModel(NetworkModel):
@@ -23,9 +24,25 @@ class LBPModel(NetworkModel):
         self._bandwidth = bandwidth
         self._pipeline = pipeline
 
-    def evaluate(self, time: Time, task: PutTask) -> NetworkTime:
+    def evaluate(self,
+                 time: Time,
+                 task: Union[PutTask, GetTask]
+                 ) -> NetworkTime:
         """
         Evaluate the latency-bandwidth network model.
+        """
+
+        if isinstance(task, PutTask):
+            return self._evaluate_put(time, task)
+
+        if isinstance(task, GetTask):
+            return self._evaluate_get(time, task)
+
+        raise RuntimeError(f"Task {task} not recognized.")
+
+    def _evaluate_put(self, time: Time, task: PutTask) -> NetworkTime:
+        """
+
         """
 
         local = self._pipeline
@@ -35,3 +52,16 @@ class LBPModel(NetworkModel):
 
         return NetworkTime(cast(Time, time + local),
                            cast(Time, time + remote))
+
+    def _evaluate_get(self, time: Time, task: GetTask) -> NetworkTime:
+        """
+
+        """
+        
+        raise NotImplementedError
+
+        # local non-blocking, cannot use buffer that is gotten
+        #   but we don't always want it blocking
+
+        # remote, modify the taken buffer
+        # local blocking, use the buffer
